4de00634ed93edae6c4d74c05d0e9ec9b44b27ee
Imkamalesh <adamkamalesh@gmail.com>
2023-09-18 17:22:11 +0530

    minor bug fixes

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Kamalesh-admin</span>
+      <span>© Kamalesh Dev Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
